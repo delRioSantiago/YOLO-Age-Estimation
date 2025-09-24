6e33e7986e464266f6e30c02b78f57041ff20c25
Santiago <santiago@example.com>
2025-09-24 20:44:35 +0200

    update body detection script

--- 01_detect_bodies.py
+++ 01_detect_bodies.py
@@ -3,9 +3,8 @@ import  csv
 import cv2
 import yaml
 from ultralytics import YOLO
-from datetime import datetime
 
-with open("config.yaml","r") as f:
+with open("config.yaml") as f:
     cfg = yaml.safe_load(f)
 
 IN_PATH   = cfg["paths"]["images"]
@@ -16,13 +15,12 @@ os.makedirs(OUT_BODIES_PATH, exist_ok=True); os.makedirs(CSV_PATH, exist_ok=True
 model = YOLO(cfg["detect"]["yolo_body"])
 IMG_SIZE = cfg["detect"]["img_size_body"]
 CONF_THRES = min(cfg["detect"]["body_conf_thres"])  
-TIMESTAMP = datetime.now().strftime("%Y-%m-%d %H:%M:%S") # Checken ob das funktioniert
 
 csv_path = os.path.join(CSV_PATH, "bodies.csv")
-write_header = not os.path.exists(csv_path)
 
 with open(csv_path, "a", newline="") as csv_file:
     csv_writer = csv.writer(csv_file)
+    write_header = not os.path.exists(csv_path)
     if write_header:
         header = cfg["schema"]["bodies"]
         csv_writer.writerow(header)
@@ -40,7 +38,7 @@ with open(csv_path, "a", newline="") as csv_file:
         body_id = 0
         for box in result.boxes:
             x1,y1,x2,y2 = map(int, box.xyxy[0].tolist())
-            x,y,w,h = x1, y1, max(0,x2-x1), max(0,y2-y1)
+            w,h = max(0,x2-x1), max(0,y2-y1)
             if w<=0 or h<=0: 
                 continue
             conf = float(box.conf[0])
@@ -50,14 +48,5 @@ with open(csv_path, "a", newline="") as csv_file:
             out_path = os.path.join(OUT_BODIES_PATH, out_name)
             cv2.imwrite(out_path, crop)
 
-            csv_writer.writerow([
-                image_id, path, W, H,
-                body_id,
-                f"{x},{y},{w},{h}",
-                f"{x/W:.6f},{y/H:.6f},{w/W:.6f},{h/H:.6f}",
-                f"{conf:.6f}",
-                out_path,
-                cfg["detect"]["yolo_body"],
-                TIMESTAMP
-            ])
+            csv_writer.writerow([image_id, body_id, f"{conf:.6f}", out_path, cfg["detect"]["yolo_body"]])
             body_id += 1
